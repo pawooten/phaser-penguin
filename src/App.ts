4f6adf5c4b2a4632ae07809bea15619322a09939
pawooten@gmail.com <pawooten@gmail.com>
2024-03-03 19:33:14 -0500

    feat: re-add phaser demo

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,29 @@
-import { writeMessage } from "./Utils";
+import Phaser from "phaser";
 
-console.log(writeMessage('Hello Webpack Typescript World'));
+class Example extends Phaser.Scene
+{
+    preload ()
+    {s
+        this.load.image('cloud', 'assets/cloud.png');
+    }
+    create ()
+    {
+        this.add.image(400, 300, 'ice');
+        // this.add.image(200, 300, 'cloud');
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x:0, y: 200 }
+        }
+    }
+};
+
+const game = new Phaser.Game(config);
